922f1c16700ac16c876d0028949ddae089e827b5
GuilhermeWilker <104400931+GuilhermeWilker@users.noreply.github.com>
2022-05-21 15:32:14 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,51 @@
+/* 
+            PRIMEIRO EXEMPLO DE [addEventListener]
+
+    const superHomem = document.getElementById('super-homem')
+
+    superHomem.addEventListener('mouseenter', () => { 
+        superHomem.classList.add('selecionado');
+    })
+*/
+
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        /*  
+            OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
+
+                    - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação.
+
+                    - retirar a classe selecionado do personagem anterior (que está selecionado).
+        */
+
+        const personagemSelecionado = document.querySelector('.selecionado')
+        personagemSelecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado')
+
+        /*
+            OBJETIVO 2 - quando passar o mouse em cima do personagem da listagem, trocar a imagem e o nome do personagem da esquerda:
+
+                    - alterar a imagem do jogador 1.
+                    
+                    - alterar o nome do jogador 1.
+        */
+          
+        const idSelecionado = personagem.attributes.id.value;
+        
+        const imagemJogador1 = document.getElementById('personagem-jogador-1');
+        imagemJogador1.src = `./src/imagens/${idSelecionado}.png`
+
+        const nomeJogador1 = document.getElementById('nome-jogador-1');
+        const nomeSelecionado = personagem.getAttribute('data-name');
+
+        nomeJogador1.innerHTML = nomeSelecionado;
+        
+    })
+})
+
+
+
